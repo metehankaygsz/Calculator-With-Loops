4e894577b535246aad67ea9328b37946ebbea599
Metehan <Metehan@DESKTOP-S3A1JGP>
2022-06-28 11:57:42 +0300

    Proje dosyası ekle.

---
+++ Calculator With Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (; ; )
+{
+    Console.WriteLine("1st Value");
+    string str1 = Console.ReadLine();
+    double int1 = Convert.ToDouble(str1);
+    Console.WriteLine("Symbol");
+    string sym = Console.ReadLine();
+    Console.WriteLine("2nd Value");
+    string str2 = Console.ReadLine();
+    double int2 = Convert.ToDouble(str2);
+
+    if (sym == "+")
+    { Console.WriteLine(int1 + int2); }
+
+    if (sym == "-")
+    { Console.WriteLine(int1 - int2); }
+
+    if (sym == "*")
+    { Console.WriteLine(int1 * int2); }
+
+    if (sym == "/")
+    { Console.WriteLine(int1 / int2); }
+    Console.WriteLine("Press enter to continue");
+    Console.ReadKey();
+    Console.Clear();
+}
